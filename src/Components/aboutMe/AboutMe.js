5d5f9785823f3d95e989ca67606f0f32eceba24e
404AP <100649990+404AP@users.noreply.github.com>
2022-03-26 18:52:32 -0400

    About me line chart complete

--- src/Components/aboutMe/AboutMe.js
+++ src/Components/aboutMe/AboutMe.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import FolderIMG from "../../Assets/folder.png"
+import WindowIMG from "../../Assets/window.png"
 import './AboutMe.css'
+
+
+
+
+
+
 
 export default class AboutMe extends Component {
   render() {
@@ -10,44 +17,113 @@ export default class AboutMe extends Component {
             <div className="aboutme-content-conatiner">
                 <div className="aboutme-left">
                     <div className="aboutme-skills">
-                        <h4 className="aboutme-skills-subheader"> Languages:</h4>
+                        <img src={WindowIMG} alt="Skills Section" className="window-background-img" />
+                        <h4 className="aboutme-skills-subheader languages"> Languages:</h4>
                         <ul className="aboutme-languages-list">
-                            <li className="aboutme-listitem">Java</li>
-                            <li className="aboutme-listitem">JS</li>
-                            <li className="aboutme-listitem">Python</li>
-                            <li className="aboutme-listitem">HTML</li>
-                            <li className="aboutme-listitem">CSS</li>
+                            <li className="aboutme-listitem java">
+                            <div class="progressbar-title">
+                                <h4>Java</h4>
+                                <div class="percent" id="java-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">85%</h5>
+                            </div>
+                            </li>
+                            <li className="aboutme-listitem js">
+                            <div class="progressbar-title">
+                                <h4>JS</h4>
+                                <div class="percent" id="js-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">60%</h5>
+                            </div>
+                            </li>
+                            <li className="aboutme-listitem python">
+                            <div class="progressbar-title">
+                                <h4>Python</h4>
+                                <div class="percent" id="python-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">70%</h5>
+                            </div>
+                            </li>
+                            <li className="aboutme-listitem html">
+                            <div class="progressbar-title">
+                                <h4>HTML</h4>
+                                <div class="percent" id="html-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">60%</h5>
+                            </div>
+                            </li>
+                            <li className="aboutme-listitem css">
+                            <div class="progressbar-title">
+                                <h4>CSS</h4>
+                                <div class="percent" id="css-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">50%</h5>
+                            </div>       
+                            </li>
                             </ul>
-                        <h4 className="aboutme-skills-subheader"> Software Skills:</h4>
+                        <h4 className="aboutme-skills-subheader SoftwareSkills"> Software Skills:</h4>
                         <ul className="aboutme-softwarelist">
-                            <li className="aboutme-listitem">Photoshop</li>
-                            <li className="aboutme-listitem">After Effects</li>
-                            <li className="aboutme-listitem">Excel</li>
+                            <li className="aboutme-listitem photoshop">
+                            <div class="progressbar-title">
+                                <h4>PS</h4>
+                                <div class="percent" id="am-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">80%</h5>
+                            </div> 
+                            </li>
+                            <li className="aboutme-listitem aftereffects">
+                            <div class="progressbar-title">
+                                <h4>AE</h4>
+                                <div class="percent" id="ae-pourcent">
+                                    <div className="value-1"></div>
+                                    </div>
+                                <h5 className="pourcent">70%</h5>
+                            </div> 
+                            </li>
                             </ul>
                         </div>
                     <div className="aboutme-holder">
                         </div>
                     <div className="aboutme-interest">
-                        <h4 className="aboutme-skills-subheader"> Interest:</h4>
+                        <img src={WindowIMG} alt="Skills Section" className="window-background-img" />
+                        <h4 className="aboutme-skills-subheader interest"> Interest:</h4>
                         <ul className="interest-list">
-                            <li className="aboutme-listitem">I really like  Economics and Finance in General</li>
+                            <li className="aboutme-listitem">I really like  Economics in General</li>
                             <li className="aboutme-listitem">Crypto is the future of finance and NFT's will change investing forever</li>
                             <li className="aboutme-listitem">I love the impact of sports more than any particular team or player</li>
                             </ul>
                         </div>
                     <div className="aboutme-summary">
-                        <h4 className="aboutme-skills-subheader"> Summary:</h4>
+                        <img src={WindowIMG} alt="Skills Section" className="window-background-img" />
+                        <h4 className="aboutme-skills-subheader summary"> Summary:</h4>
                         <p className="aboutme-summary-p">
                             I specialize in frontend development but I think it is important to be a Full Stack Engineer . Creating and Implementing UI's down to very fine details is very fulfilling to say the least. Whenever I can find free time I like to work on small projects to help me get better at building and <span className="aboutme-summary-wordhype">  &lt;/styling&gt;   </span> websites. These great looking website are even better when they have a robust and secure backend to power the application.
                             </p>
                         </div>
                     </div>
                 <div className="aboutme-right">
-                    <img src={FolderIMG} alt="Download Resume" className="aboutme-foldericon" />
-                    <button className="aboutme-download-button">Download</button> 
-                    </div>
+
+                    <div className="aboutme-folder-container">
+                        <img src={FolderIMG} alt="Download Resume" className="aboutme-foldericon" />
+                        <p className="folder-cap">
+                            Take a Copy of my Resume.
+                            
+                            </p>
+                        <button className="aboutme-download-button">Download</button> 
+                        </div>
+                        
+
+                        </div>
+                  
                 </div>
         </div>
-    )
+        )
   }
 }
+
