09bdfbe39eb6a4bddb7dd1e5a494018e34c66053
404AP <100649990+404AP@users.noreply.github.com>
2022-06-30 22:47:45 -0400

    Portfolio Section
    Added some stuff to the portfolio section including working buttons

--- src/Components/portfolio/Portfolio.js
+++ src/Components/portfolio/Portfolio.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react'
 import Projects from './Projects'
 import './Portfolio.css'
 import eyesIMG from '../../Assets/eyes.png'
-import previewIMG from '../../Assets/preview.png'
+import WindowIMG from "../../Assets/window.png"
+import powershellIMG from '../../Assets/powershell_icon.png'
+import grindsetglobalThumbIMG from '../../Assets/grindsetglobal_thumb.png'
+import speedyIMG from '../../Assets/previewSpeedy.png'
+import ggIMG from '../../Assets/previewGG.png'
+import { set } from 'react-hook-form'
+// import speedyIMG from '../../Assets/speedy.png'
 
 export default class Portfolio extends Component {
     constructor(props) {
@@ -10,68 +16,68 @@ export default class Portfolio extends Component {
         this.state = {
           items:[
             {text: 'xxx',
-            img:"",
+            thumbnail: powershellIMG,
+            preview: speedyIMG,
+            description:"speedy",
             key:"0",
-            title:"SpeedyIntegrationScript"}
+            title:"Speedy Integration Tool"}
             ,
             {text: 'xxx',
-            img:"",
+            thumbnail: grindsetglobalThumbIMG,
+            preview: ggIMG,
+            description:"gg",
             key:"1",
             title:"Grindset Global"}
+
          ],
-         
+
           currentItem:{
             text:'',
+            description:"",
             img: '',
             key:''
           },
           hasError: false
         };
 
+        this.setCurrentItem = this.setCurrentItem.bind(this);
 
     }
     
-    setCurrentItem = () => {        
-        const newItem = this.state.items[0];
+    setCurrentItem(item) {
+        const newItem = (!(typeof item === 'undefined' || item === null)) ? this.state.items[item] : this.state.items[0];
+        console.log(item);
         this.setState({
             currentItem: newItem
         });
-        if (this.state.currentItem.title) {
-            this.loadProjects();
-        }
+    }
+    componentDidMount() {
+        this.setCurrentItem();
     }
 
-    setPreview = () => {
-
-    }
-
-    loadProjects = () => {
-        //take projects out of items and make elements (list items)from them
-        console.log('one');
-    }
 
   render() {
     return (
-        <div onLoad={this.setCurrentItem} className="portfolio-container">
+        <div style={{backgroundImage: 'url(' + WindowIMG + ')'}} className="portfolio-container">
             <h1 className="portfolio-header"> Portfolio</h1>
             <div className="portfolio-content-container">
                 <div className="portfolio-left">
                     <h2 className="portfolio-projects-header">Projects</h2>
                     <div className='project-list-container'>
                         <ul className="project-examples-list">
-                            <Projects items={this.state.items} ></Projects>
+                            <Projects settingCurrentItem={this.setCurrentItem} items={this.state.items}></Projects>
                         </ul>
                     </div>                
                 <p className="contact-me-cta">Interested in Working Together? <br /> <span className='portfolio-contactme-wordhype'>Contact Me</span></p>
                 <img src={eyesIMG} alt="alvanprice.com" className="portfolio-eyes" />            
                 </div>
-            <div className="portfolio-right">
+            <div  className="portfolio-right">
                 <div className="portfolio-preview-container">
-                    <img src={previewIMG} alt="example project" className="preview-image" />
+                    <img src={this.state.currentItem.preview} alt="example project" className="preview-image" />
                     </div>
                 <div className="site-des-container">
-                    <h1 className="portfolio-site-description-header">SITE DESCRIPTION -</h1>
-                    <p className="portfolio-site-details"> Portfolio Site Details</p>
+                    <h1 className="portfolio-site-description-header">SITE DESCRIPTION: {this.state.currentItem.title}</h1>
+                    <p className="portfolio-site-details"> {this.state.currentItem.description} </p>
                     </div>
                 </div>
             </div>
--- src/Components/portfolio/Projects.js
+++ src/Components/portfolio/Projects.js
@@ -5,14 +5,23 @@ import './Projects.css';
 
 
 
-
-
-
 function Projects(props) {
-
     const items = props.items;
+
+    
+   
     const listItems = items.map( item => {
-        return <div key={item.key} className={'project-icon-container'} id={'project-icon-container'+ item.key}>
+        var image = {backgroundImage: 'url(' + item.thumbnail + ')'};
+        return <div 
+        onClick={(e) => {
+            e.preventDefault();
+            props.settingCurrentItem(item.key)}} 
+        key={item.key} 
+        className={'project-icon-container'} 
+        id={'project-icon-container'+ item.key}>
+            <div style={image} className={'project-icon' + item.key + " icons"}>
+
+            </div>
             <h5>{item.title}</h5>
         </div>
     });
