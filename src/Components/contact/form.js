c9292f4cfe9ed20303cc2228c90ae7f49a4f47c4
404AP <100649990+404AP@users.noreply.github.com>
2022-07-29 17:33:56 -0400

    Untested Fix for Keyboard

--- src/Components/contact/form.js
+++ src/Components/contact/form.js
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
-import useFormHero from "../../formHero";
-import { useForm } from "react-hook-form";
+import React, { useRef } from 'react';
+import useFormHero from '../../formHero';
+import { useForm } from 'react-hook-form';
 
-const FORM_ENDPOINT = "https://public.herotofu.com/v1/8808a2f0-018c-11ed-af3d-615a7cafe262"; // TODO - fill on the later step
+const FORM_ENDPOINT =
+  'https://public.herotofu.com/v1/8808a2f0-018c-11ed-af3d-615a7cafe262'; // TODO - fill on the later step
 
 const Form = () => {
   const formElement = useRef(null);
@@ -10,15 +11,19 @@ const Form = () => {
     sent: new Date().toISOString(),
   };
 
-  const { register, handlingSubmit, watch, formState: { errors } } = useForm();
-
+  const {
+    register,
+    handlingSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
   const { handleSubmit, status, message } = useFormHero({
     form: formElement.current,
     additionalData,
   });
 
-  if (status === "success") {
+  if (status === 'success') {
     return (
       <>
         <div>Thank you!</div>
@@ -27,7 +32,7 @@ const Form = () => {
     );
   }
 
-  if (status === "error") {
+  if (status === 'error') {
     return (
       <>
         <div>Something bad happened!</div>
@@ -44,32 +49,87 @@ const Form = () => {
       target="_blank"
       ref={formElement}
     >
-        <div className="contact-form-top">
-            <label htmlFor="name" className="contact-name-label"  > Name:</label>
-            <br />
-            <input   className='name-input' id='name'   required {...register("Name" , { required: true })}/>
-            <br />
-            <label htmlFor="email" className="contact-email-label" > Email:</label>
-            <br />
-            <input  type='email' className='email-input' id='email' required {...register("Email" , { required: true })}/>
-            <br />
-            <label htmlFor="options">I would like to talk more about :</label>
-            <br />
-            <fieldset type="radio" className="contact-radio-buttons" id='options' required>
-                <input type="radio" className="option-1" value='website' name='options'  {...register("reason" , { required: true })}/>
-                <label htmlFor="website" className="contact-option-label">Building a Website/ Web App</label>
-                <br />
-                <input type="radio" className="option-2" value="consulting" name='options' {...register("reason" , { required: true })}/>
-                <label htmlFor="consulting" className="contact-option-label">Consulting</label>
-                <br />
-                <input type="radio" className="option-3" value="job" name='options' {...register("reason" , { required: true })}/>
-                <label htmlFor="job" className="contact-option-label">A Job Offer </label>
-            </fieldset>
-           
-            </div>  
-            <div className="contact-form-bottom">
-            <input type="submit" />
-        </div>
+      <div className="contact-form-top">
+        <label htmlFor="name" className="contact-name-label">
+          Name:
+        </label>
+        <br />
+        <input
+          className="name-input"
+          id="name"
+          required
+          {...register('Name', { required: true })}
+        />
+        <br />
+        <label htmlFor="email" className="contact-email-label">
+          Email:
+        </label>
+        <br />
+        <input
+          type="email"
+          className="email-input"
+          id="email"
+          required
+          {...register('Email', { required: true })}
+        />
+        <br />
+        <label htmlFor="tel" className="contact-number-label">
+          Phone Number:
+        </label>
+        <br />
+        <input
+          type="tel"
+          className="tel-input"
+          id="tel"
+          required
+          {...register('Phone Number', { required: true })}
+        />
+        <br />
+        <label htmlFor="options">I would like to talk more about :</label>
+        <br />
+        <fieldset
+          type="radio"
+          className="contact-radio-buttons"
+          id="options"
+          required
+        >
+          <input
+            type="radio"
+            className="option-1"
+            value="website"
+            name="options"
+            {...register('reason', { required: true })}
+          />
+          <label htmlFor="website" className="contact-option-label">
+            Building a Website/ Web App
+          </label>
+          <br />
+          <input
+            type="radio"
+            className="option-2"
+            value="consulting"
+            name="options"
+            {...register('reason', { required: true })}
+          />
+          <label htmlFor="consulting" className="contact-option-label">
+            Consulting
+          </label>
+          <br />
+          <input
+            type="radio"
+            className="option-3"
+            value="job"
+            name="options"
+            {...register('reason', { required: true })}
+          />
+          <label htmlFor="job" className="contact-option-label">
+            A Job Offer{' '}
+          </label>
+        </fieldset>
+      </div>
+      <div className="contact-form-bottom">
+        <input type="submit" />
+      </div>
     </form>
   );
 };
